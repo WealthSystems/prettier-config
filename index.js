f7c87e462fb57626a1b6016ae459d5723cf2d0f2
Rafael Rubenich <rfrubenich@gmail.com>
2019-04-24 17:37:58 -0300

    feat: initial configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: false,
+    endOfLine: "lf",
+    htmlWhitespaceSensitivity: "strict",
+    insertPragma: false,
+    jsxBracketSameLine: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    proseWrap: "never",
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: false
+};
