e5102f698fd6a386e6f422d6ad9f5b9e87c00724
Rafael Rubenich <rfrubenich@gmail.com>
2019-04-26 11:16:50 -0300

    fix: remove double quotes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 module.exports = {
-    arrowParens: "always",
+    arrowParens: 'always',
     bracketSpacing: false,
-    endOfLine: "lf",
-    htmlWhitespaceSensitivity: "strict",
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'strict',
     insertPragma: false,
     jsxBracketSameLine: true,
     jsxSingleQuote: false,
     printWidth: 100,
-    proseWrap: "never",
+    proseWrap: 'never',
     requirePragma: false,
     semi: true,
     singleQuote: true,
     tabWidth: 4,
-    trailingComma: "all",
+    trailingComma: 'all',
     useTabs: false
 };
